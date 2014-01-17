ce10b1011b7e21acf4a0b659527b8c3222a20006
NeedMoreDesu <desu@horishniy.org.ua>
2014-01-17 16:18:42 +0200

    Initial Commit

---
+++ m7background/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  m7background
+//
+//  Created by dev on 1/17/14.
+//  Copyright (c) 2014 dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ m7backgroundTests/m7backgroundTests.m
@@ -0,0 +1,34 @@
+//
+//  m7backgroundTests.m
+//  m7backgroundTests
+//
+//  Created by dev on 1/17/14.
+//  Copyright (c) 2014 dev. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface m7backgroundTests : XCTestCase
+
+@end
+
+@implementation m7backgroundTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
